46cb7d950e84a174cdd437cddec80e2a54409143
Daniel Abanto <daniel4.ah@gmail.com>
2020-09-08 17:38:00 -0500

    Cambios al fondo y detalles visuales

--- src/containers/Somos.js
+++ src/containers/Somos.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './scss/Somos.scss'
-import foto from '../img/Marco_playa.jpg'
 
 const Somos = () => {
 	return(
 		<div className="Somos_container" id="somos">
 			<figure>
-				<img className="Somos_img"src={foto} alt="auto"/>
+				<img className="Somos_img"src="https://www.bolsamania.com/seriesadictos/wp-content/uploads/2011/11/rob-schneider-uk-premiere-bedtime-stories-01-450x634.jpg" alt="auto"/>
 			</figure>
 			<div className="Somos_details">
-				<h2 className="Somos_title">Nosotros</h2>
-				<p className="Somos_p"><b>En Premium Mr. Gordem Escuela llevamos 3 años brindando un servicio personalizado y de calidad</b> formando a nuestros estudiantes para ser conductores con los más altos estandares cumpliendo en su totalidad con todo lo que la reglamentacion demanda y ...  </p>
+				<h2 className="Somos_title">NOSOTROS</h2>
+				<p className="Somos_p">En Premium Mr. Gordem Escuela llevamos 3 años brindando un servicio personalizado y de calidad formando a nuestros estudiantes para ser conductores con los más altos estandares cumpliendo en su totalidad con todo lo que la reglamentacion demanda y ...  </p>
 			</div>
 		</div>
 	)
