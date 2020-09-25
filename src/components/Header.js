577b9458b8ca947e2469592e837b5e4467803e00
Daniel Abanto <daniel4.ah@gmail.com>
2020-09-24 23:28:30 -0500

    Agregando hamburger button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React, { Fragment, useState, useEffect } from 'react'
 import './scss/Header.scss'
+import menu from '../img/menu.svg'
 
 const Header = () => {
     const [somos, setSomos] = useState('')
     const [servicios, setServicios] = useState('')
+
     useEffect(() => {
         cambiar();
         addEventListener('resize', cambiar)
     }, [window.innerWidth])
     const cambiar = () => {
+        
+        const menu = document.getElementById('menu')
         if (window.innerWidth<768) {
             setSomos('NOSOSTROS')
             setServicios('SERVICIOS')
@@ -16,6 +20,16 @@ const Header = () => {
             setSomos('QUIENES SOMOS')
             setServicios('NUESTROS SERVICIOS')
         }
+        if (window.innerWidth<481){
+            document.getElementById('navbar_list').classList.add('none')
+        }
+    }
+    
+    const handleClickImg = (e) =>{
+        document.getElementById('navbar_list').classList.toggle('none')
+    }
+    const handleClick = (e) =>{
+        document.getElementById('navbar_list').classList.toggle('none')
     }
     return(
         <Fragment>
@@ -24,14 +38,16 @@ const Header = () => {
             <h2>ESCUELA DE CONDUCTORES</h2>
         </header>
         <div className="navbar">
-            <ul>
-                <li><a href="#home">HOME</a></li>
-                <li><a href="#somos">{somos}</a></li>
-                <li><a href="#servicios">{servicios}</a></li>
-                <li><a href="#ubicanos">UBICANOS</a></li>
+            <img src={menu} onClick={handleClickImg} className="hamburger_menu" id="menu"/>
+            <ul id="navbar_list" className="navbar_list">
+                <li><a onClick={handleClick} className="navbar_element" href="#home">HOME</a></li>
+                <li><a onClick={handleClick} className="navbar_element" href="#somos">{somos}</a></li>
+                <li><a onClick={handleClick} className="navbar_element" href="#servicios">{servicios}</a></li>
+                <li><a onClick={handleClick} className="navbar_element" href="#ubicanos">UBICANOS</a></li>
             </ul>
         </div>
         </Fragment>
-    )}
+    )
+    }
 
 export default Header
